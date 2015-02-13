ffc0eb85981db4e07467621078081bf2232c2598
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-13 15:26:02 -0600

    added passing test on object properties

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -6,3 +6,25 @@ describe('test suite', function () {
     false.should.be.false;
   });
 });
+
+describe('DOM', function () {
+  describe('div', function(){
+    beforeEach(function() {
+      if (window.__karma__) {
+        $('body').append('<div class=".candidatePool"></div>');
+      }
+    });
+    beforeEach(function() {
+      $('div').empty();
+    });
+  });
+
+  describe('animal object', function () {
+    it('should have property', function () {
+      var animal = { location: 'Nashville', name: 'Felix', occupation: 'nurse', sex: 'Male' };
+      expect(animal).to.have.property('location');
+    });
+  });
+
+});
+
